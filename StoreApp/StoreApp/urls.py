0e280e6b5a32038ba591357d844d3475696f3197
MrKoBo <kdtate4@memphis.edu>
2024-04-20 20:09:00 -0500

    Realized I missed named my url but added hyperlink to my show

--- StoreApp/StoreApp/urls.py
+++ StoreApp/StoreApp/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='product_list'),
+    path('products/', views.product_list, name='products'),
 ]
 urlpatterns += [
     path('products/<int:id>/', views.product_show, name='product_show'),
